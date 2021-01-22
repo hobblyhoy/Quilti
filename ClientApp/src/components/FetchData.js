50ec63bbd85ea3a4347d7880390c6e26b33f2e41
Nathan <nathan@hobblyhoy>
2021-01-21 18:16:27 -0800

    convert to latest with hooks

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -1,59 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 
-export class FetchData extends Component {
-  static displayName = FetchData.name;
+export function FetchData() {
+   const [forecasts, setForecasts] = useState([]);
+   const [loading, setLoading] = useState(true);
 
-  constructor(props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
-  }
+   useEffect(() => {
+      populateWeatherData();
+   }, []);
 
-  componentDidMount() {
-    this.populateWeatherData();
-  }
+   let  populateWeatherData = async () => {
+      const response = await fetch('weatherforecast');
+      const data = await response.json();
+      setForecasts(data);
+      setLoading(false);
+   }
 
-  static renderForecastsTable(forecasts) {
-    return (
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
-  }
+   let renderForecastsTable = (forecasts) => {
+      return (
+         <table className='table table-striped' aria-labelledby="tabelLabel">
+            <thead>
+               <tr>
+                  <th>Date</th>
+                  <th>Temp. (C)</th>
+                  <th>Temp. (F)</th>
+                  <th>Summary</th>
+               </tr>
+            </thead>
+            <tbody>
+               {forecasts.map(forecast =>
+                  <tr key={forecast.date}>
+                     <td>{forecast.date}</td>
+                     <td>{forecast.temperatureC}</td>
+                     <td>{forecast.temperatureF}</td>
+                     <td>{forecast.summary}</td>
+                  </tr>
+               )}
+            </tbody>
+         </table>
+      );
 
-  render() {
-    let contents = this.state.loading
+   }
+
+   let contents = loading
       ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
+      : renderForecastsTable(forecasts);
 
-    return (
+   return (
       <div>
-        <h1 id="tabelLabel" >Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
-        {contents}
+         <h1 id="tabelLabel" >Weather forecast</h1>
+         <p>This component demonstrates fetching data from the server.</p>
+         {contents}
       </div>
-    );
-  }
+   );
 
-  async populateWeatherData() {
-    const response = await fetch('weatherforecast');
-    const data = await response.json();
-    this.setState({ forecasts: data, loading: false });
-  }
+
+
 }
